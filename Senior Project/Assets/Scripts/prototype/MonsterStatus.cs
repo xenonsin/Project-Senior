0aa3ed8d22c3a646cf22fe2ff5b733e1fcf6fea4
Andrew Kwong <ahkwong@cpp.edu>
2015-12-09 06:31:36 -0800

    Updated MonsterStatus Script, added aggroDetector, floating healthbar

--- Senior Project/Assets/Scripts/prototype/MonsterStatus.cs
+++ Senior Project/Assets/Scripts/prototype/MonsterStatus.cs
@@ -1,19 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class MonsterStatus : MonoBehaviour {
 
-    public int health = 100;
+    private GameObject aggroRange;    
     private Animation anim;
-    //private float distance;
-    private float rotateSpeed = 10f;
     private Vector3 spawnLocation;
-    //private float movespeed = 0.25f;// * Time.deltaTime;
-    private float aggroRadius = 2f;
-    //private float meleerange = 0.5f;
-    //private Vector3 playerpos = Vector3.zero;
-    //private bool attackcooldown = false;
-    //private bool dmganimation = false;
+    private float rotateSpeed = 10f;
+    private float movespeed = 0.5f;
+    public GameObject playerInRange;
+    public float aggroRadius = 2f;
+    public float distanceToPlayer;
+    private float meleeRange = 0.7f;
+    public float maxHealth = 100f;
+    public float currentHealth;
 
     private enum State
     {
@@ -36,15 +37,15 @@ public class MonsterStatus : MonoBehaviour {
             case State.Run:
                 run_state();
                 break;
-            //case State.Attack:
-            //    atk_state();
-            //    break;
-            //case State.TakeDmg:
-            //    dmg_state();
-            //    break;
-            //case State.Dead:
-            //    dead_state();
-            //    break;
+            case State.Attack:
+                atk_state();
+                break;
+            case State.TakeDmg:
+                dmg_state();
+                break;
+            case State.Dead:
+                dead_state();
+                break;
         }
     }
 
@@ -63,67 +64,71 @@ public class MonsterStatus : MonoBehaviour {
         //animation
         anim.Play("Idle");
 
-    //    //move towards the player until melee range
-    //    if (distance > meleerange)
-    //        transform.position = Vector3.Lerp(transform.position, playerpos, movespeed);
-    //    else
-    //        currentState = State.Attack;
-
         //if player is out of aggro range, go back to idle
         if (playerInRange == null)
             currentState = State.Idle;
         else
         {
-            //look at the player
+            //look at the targeted player
             Vector3 pos = playerInRange.transform.position - transform.position;
             Quaternion newRotation = Quaternion.LookRotation(pos);
             transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+
+            //move towards the targeted player and switch to attack
+            if (distanceToPlayer > meleeRange)
+                transform.position = Vector3.MoveTowards(transform.position, playerInRange.transform.position, movespeed * Time.deltaTime);
+            else
+                currentState = State.Attack;
         }
     }
 
-    //void atk_state()
-    //{
-    //    //attack once
-    //    if (!attackcooldown)
-    //    {
-    //        anim["Attack"].speed = 0.5f;
-    //        anim.Play("Attack");
-    //        attackcooldown = true;
-    //    }
+    private bool attackAnimation = false;
 
-    //    //if player is out of melee range, go to run state
-    //    if (distance > meleerange)
-    //        currentState = State.Run;
+    void atk_state()
+    {
+        //attack once
+        if (!attackAnimation)
+        {
+            anim["Attack"].speed = 0.5f;
+            anim.Play("Attack");
+            attackAnimation = true;
+        }
 
-    //    //reset attackcooldown
-    //    if(!anim.IsPlaying("Attack"))
-    //        attackcooldown = false;
-    //}
+        //if player is out of melee range, go to run state
+        if (distanceToPlayer > meleeRange)
+            currentState = State.Run;
 
-    //void dmg_state()
-    //{
-    //    if (!dmganimation)
-    //    {
-    //        anim.Play("TakeDmg");
-    //        dmganimation = true;
-    //    }
-    //    if (!anim.IsPlaying("TakeDmg"))
-    //    {
-    //        currentState = State.Idle;
-    //        dmganimation = false;
-    //    }
-    //}
+        //reset attackanimation
+        if(!anim.IsPlaying("Attack"))
+            attackAnimation = false;
+    }
 
-    private bool deadanimation = false;
+    private bool dmgAnimation = false;
+
+    void dmg_state()
+    {
+        if (!dmgAnimation)
+        {
+            anim.Play("TakeDmg");
+            dmgAnimation = true;
+        }
+        if (!anim.IsPlaying("TakeDmg"))
+        {
+            currentState = State.Idle;
+            dmgAnimation = false;
+        }
+    }
+
+    private bool deadAnimation = false;
 
     void dead_state()
     {
         //play dead animation once
-        if (!deadanimation)
+        if (!deadAnimation)
         {
             anim["Die"].speed = 0.5f;
             anim.Play("Die");
-            deadanimation = true;
+            deadAnimation = true;
         }
 
         //destroy gameobject after dead animation is finished
@@ -131,42 +136,26 @@ public class MonsterStatus : MonoBehaviour {
             Destroy(gameObject);
     }
 
-    private GameObject aggroRange;
-    public GameObject playerInRange;
-
     void Awake()
     {
-        //initialization and creating aggroDetector
+        //initializing healthbar
+        currentHealth = maxHealth;
+        backgroundHealthBar.SetPixel(0, 0, Color.black);
+        backgroundHealthBar.Apply();
+        foregroundHealthBar.SetPixel(0, 0, Color.red);
+        foregroundHealthBar.Apply();
+
+        //initializing animation component
         anim = GetComponent<Animation>();
+
+        //initializing aggroDetector
         aggroRange = new GameObject("aggroRange");
         aggroRange.transform.position = gameObject.transform.position;
         aggroRange.transform.SetParent(gameObject.transform);
         aggroRange.AddComponent<SphereCollider>();
         aggroRange.GetComponent<SphereCollider>().isTrigger = true;
         aggroRange.GetComponent<SphereCollider>().radius = aggroRadius;
-
-        //recording spawnLocation
-        spawnLocation = gameObject.transform.position;
     }
-
-    //aggroDetector Behaviors
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.tag == "Player")
-            if(playerInRange==null)
-                playerInRange = col.gameObject;
-    }
-    //void OnTriggerStay(Collider col)
-    //{
-    //    if (playerInRange==null)
-    //        if (col.tag == "Player")
-    //            playerInRange = col.gameObject;
-    //}
-    //void OnTriggerExit(Collider col)
-    //{
-    //    if (col.gameObject == playerInRange)
-    //        playerInRange = null;
-    //}
 
     void FixedUpdate ()
     {
@@ -174,25 +163,61 @@ public class MonsterStatus : MonoBehaviour {
         StateMachine();
         Debug.Log(currentState);
 
+        if (playerInRange!=null)
+            distanceToPlayer = Mathf.Sqrt((Mathf.Abs(gameObject.transform.position.x - playerInRange.transform.position.x))
+                                        + (Mathf.Abs(gameObject.transform.position.z - playerInRange.transform.position.z)));
+
         //go to dead state from any state if health is less than 1
-        if (health < 1)
+        if (currentHealth < 1)
         {
             Debug.Log(gameObject.name + " Dead");
             currentState = State.Dead;
         }
 
-        Debug.Log(gameObject.name + " " + health);
-    //    //Debug.Log(gameObject.name + " " + currentState);
+        Debug.Log(gameObject.name + " " + currentHealth);
+
+        //healthbar GUI
+        healthbarGUI();
     }
 
-    ////calls when a trigger hit this object
-    //void OnTriggerEnter (Collider col)
-    //{
-    //    //checking if it is a bullet
-    //    if (col.tag == "Bullet")
-    //    {
-    //        currentState = State.TakeDmg;
-    //        health -= 20;
-    //    }
-    //}
+    //aggroDetector Behaviors - Follows one person forever
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+            if (playerInRange == null)
+                playerInRange = col.gameObject;
+    }
+
+    //calls when a trigger hit this object
+    void OnCollisionEnter(Collision col)
+    {
+        //checking if it is a bullet
+        if (col.gameObject.tag == "Bullet")
+        {
+            currentState = State.TakeDmg;
+            currentHealth -= 20f;
+        }
+    }
+
+
+    //healthbar GUI
+    private Texture2D backgroundHealthBar = new Texture2D(1, 1);
+    private Texture2D foregroundHealthBar = new Texture2D(1, 1);
+    private float barLeft, barTop, barWidth = 30f, barHeight = 5f;
+    private Vector2 barPos;
+    private float healthPercent;
+
+    void healthbarGUI()
+    {
+        barPos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        barLeft = barPos.x - 10f;
+        barTop = (Screen.height - barPos.y) - 30f;
+        healthPercent = currentHealth / maxHealth;
+    }
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(barLeft, barTop, barWidth, barHeight), backgroundHealthBar);
+        GUI.DrawTexture(new Rect(barLeft, barTop, barWidth * healthPercent, barHeight), foregroundHealthBar);
+    }
+
 }
